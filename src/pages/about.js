df446c224abc5cd992aec8c7cc0627105d1e0342
Carl Avidano <cavidano@gmail.com>
2021-12-11 07:10:55 -0500

    added meta data

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { Link } from 'gatsby';
+
 function About() {
     return (
         <main>
 
         <h1>About</h1>
+        <Link to="/">
+            Back to Home
+        </Link>
             
         </main>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 function Home() {
     return (
         <main>
 
-        <h1>Hello World</h1>
-            
+            <h1>Hello World</h1>
+
+            <Link to="/about">
+                About this Site
+            </Link>
+
         </main>
     )
 }
